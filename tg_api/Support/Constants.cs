d37830de5cde9ecdcfd6bacdea61df85d5e0f1ad
Andrii Hrytsenko <73461908+anryts@users.noreply.github.com>
2022-08-07 21:50:45 +0300

    Add files via upload

---
+++ tg_api/Support/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace tg_api.Support
+{
+    static class API_KEYS
+    {
+      
+        public static string _app_key = "5cf10e177dc05e3957298e8121ee7922";
+        public static string _app_id = "6c41fe93";
+        public static string _google_translate_api = "AIzaSyCf3vMKkpT1Z9ifF3z-1-JvizzCO-uatPY";
+
+    }
+
+    static class Links
+    {
+        public static string _dict_address = "https://od-api.oxforddictionaries.com/api/v2/";
+        public static string _mongo_link = "mongodb+srv://anryts:wer12345@cluster0.vaskk.mongodb.net/?retryWrites=true&w=majority";
+    }
+}
